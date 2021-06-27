fb062e10af4af6d50dae4ffc8a80859bc1576287
shantha7kumar <76446867+shantha7kumar@users.noreply.github.com>
2021-06-27 14:54:12 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>JavaScript and ReactJS</h1>
+      <p>I got to have hands on experience on:</p>
+      <li>HTML</li>
+      <li>JavaScript</li>
+      <li>React.js</li>
+      <p>
+        during these 7 days, and everything was explained from the very basics
+        so that anyone with zero experience on programming can learn.
+      </p>
+    </div>
+  );
+}
+
+export default Info;
